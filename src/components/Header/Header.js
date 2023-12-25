d7b7764c0b5bdf6b6115885cf997b5ef8c78c0e4
leonphuc <leonphuc@gmail.com>
2023-12-25 21:07:56 +0700

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,8 @@
 'use client';
-import Link from 'next/link';
-
-import { Container, Form, Nav, Navbar, NavDropdown, Offcanvas, Button } from 'react-bootstrap';
 import React, { useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { Container, Form, Nav, Navbar, NavDropdown, Offcanvas, Button, Col, Row } from 'react-bootstrap';
 
 import './styles.scss';
 import styles from './Header.module.scss';
@@ -10,11 +10,14 @@ import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
 export default function Header() {
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const router = useRouter();
+
   return (
     <>
       <header className={cx('header')}>
-        <Container>
-          <Navbar expand="md">
+        <Container fluid="md">
+          <Navbar expand="md" className={cx('navbar-header')}>
             <Link href="/" className={cx('navbar-brand', 'header-logo')}>
               Nail Shop
             </Link>
@@ -32,53 +35,7 @@ export default function Header() {
                   <NavDropdown
                     renderMenuOnMount={true}
                     title={
-                      <span className={cx('span-icons')}>
-                        Home<i className="bi bi-chevron-down"></i>
-                      </span>
-                    }
-                    id="basic-nav-dropdown"
-                    className={cx('btn-drop-down')}
-                  >
-                    <NavDropdown
-                      renderMenuOnMount={true}
-                      title={
-                        <span className={cx('span-icons')}>
-                          Home 1 - 3 <i className="bi bi-chevron-right"></i>
-                        </span>
-                      }
-                      id="basic-nav-dropdown"
-                      drop="end"
-                      className={cx('nav-list-parent', 'border-0')}
-                    >
-                      <div className={cx('wrapper-list-child')}>
-                        <NavDropdown.Item href="#service1">Home 1</NavDropdown.Item>
-                        <NavDropdown.Item href="#service1">Home 2</NavDropdown.Item>
-                        <NavDropdown.Item href="#service1">Home 3</NavDropdown.Item>
-                      </div>
-                    </NavDropdown>
-
-                    <NavDropdown
-                      renderMenuOnMount={true}
-                      title={
-                        <span className={cx('span-icons')}>
-                          Home 4 - 6 <i className="bi bi-chevron-right"></i>
-                        </span>
-                      }
-                      id="basic-nav-dropdown"
-                      drop="end"
-                      className={cx('nav-list-parent')}
-                    >
-                      <div className={cx('wrapper-list-child')}>
-                        <NavDropdown.Item href="#service1">Home 4</NavDropdown.Item>
-                        <NavDropdown.Item href="#service1">Home 5</NavDropdown.Item>
-                        <NavDropdown.Item href="#service1">Home 6</NavDropdown.Item>
-                      </div>
-                    </NavDropdown>
-                  </NavDropdown>
-                  <NavDropdown
-                    renderMenuOnMount={true}
-                    title={
-                      <span className={cx('span-icons')}>
+                      <span className={cx('span-icons', 'list-title')}>
                         Home<i className="bi bi-chevron-down"></i>
                       </span>
                     }
@@ -125,7 +82,83 @@ export default function Header() {
                   <NavDropdown
                     renderMenuOnMount={true}
                     title={
-                      <span className={cx('span-icons')}>
+                      <span className={cx('span-icons', 'list-title')}>
+                        Shop<i className="bi bi-chevron-down"></i>
+                      </span>
+                    }
+                    id="basic-nav-dropdown"
+                    className={cx('btn-drop-down')}
+                  >
+                    <div className={cx('nav-list-parent')}>
+                      <Row>
+                        <div className={cx('wrap-list')}>
+                          <Col lg={6}>
+                            <div className={cx('mega-menu')}>
+                              <h2 className={cx('title')}>Shop pages</h2>
+                              <NavDropdown.Item href="#service1">Shop default</NavDropdown.Item>
+                              <NavDropdown.Item href="#service1">Shop list</NavDropdown.Item>
+                              <NavDropdown.Item href="#service1">My account</NavDropdown.Item>
+                              <NavDropdown.Item href="#service1">Cart</NavDropdown.Item>
+                              <NavDropdown.Item href="#service1">Checkout</NavDropdown.Item>
+                            </div>
+                          </Col>
+                          <Col lg={6}>
+                            <div className={cx('mega-menu')}>
+                              <h2 className={cx('title')}>Product detail</h2>
+                              <NavDropdown.Item href="#service1">Product default</NavDropdown.Item>
+                              <NavDropdown.Item href="#service1">Product group</NavDropdown.Item>
+                              <NavDropdown.Item href="#service1">Product extra link</NavDropdown.Item>
+                              <NavDropdown.Item href="#service1">Product variable</NavDropdown.Item>
+                              <NavDropdown.Item href="#service1">Special product</NavDropdown.Item>
+                            </div>
+                          </Col>
+                        </div>
+                      </Row>
+                    </div>
+                  </NavDropdown>
+
+                  <NavDropdown
+                    renderMenuOnMount={true}
+                    title={
+                      <span className={cx('span-icons', 'list-title')}>
+                        Blog<i className="bi bi-chevron-down"></i>
+                      </span>
+                    }
+                    id="basic-nav-dropdown"
+                    className={cx('btn-drop-down')}
+                  >
+                    <div className={cx('nav-list-parent')}>
+                      <Row>
+                        <div className={cx('wrap-list')}>
+                          <Col lg={6}>
+                            <div className={cx('mega-menu')}>
+                              <h2 className={cx('title')}>Blog pages</h2>
+                              <NavDropdown.Item href="#service1">Blog default</NavDropdown.Item>
+                              <NavDropdown.Item href="#service1">Blog list 2</NavDropdown.Item>
+                              <NavDropdown.Item href="#service1">Blog list 3</NavDropdown.Item>
+                              <NavDropdown.Item href="#service1">Blog Grid</NavDropdown.Item>
+                              <NavDropdown.Item href="#service1">Blog Grid 2</NavDropdown.Item>
+                            </div>
+                          </Col>
+                          <Col lg={6}>
+                            <div className={cx('mega-menu')}>
+                              <h2 className={cx('title')}>Latest posts</h2>
+                              <NavDropdown.Item href="#service1">Product default</NavDropdown.Item>
+                              <NavDropdown.Item href="#service1">Product group</NavDropdown.Item>
+                              <NavDropdown.Item href="#service1">Product extra link</NavDropdown.Item>
+                              <NavDropdown.Item href="#service1">Product variable</NavDropdown.Item>
+                              <NavDropdown.Item href="#service1">Special product</NavDropdown.Item>
+                            </div>
+                          </Col>
+                        </div>
+                      </Row>
+                    </div>
+                  </NavDropdown>
+
+                  <NavDropdown
+                    renderMenuOnMount={true}
+                    title={
+                      <span className={cx('span-icons', 'list-title')}>
                         Pages<i className="bi bi-chevron-down"></i>
                       </span>
                     }
@@ -162,7 +195,6 @@ export default function Header() {
                       className={cx('nav-list-parent')}
                     >
                       <div className={cx('wrapper-list-child')}>
-                        {/* <NavDropdown.Item> */}
                         <Link
                           data-rr-ui-dropdown-item=""
                           className="dropdown-item"
@@ -172,20 +204,38 @@ export default function Header() {
                         >
                           Contact 1
                         </Link>
-                        {/* </NavDropdown.Item> */}
                         <NavDropdown.Item>Contact 2</NavDropdown.Item>
                         <NavDropdown.Item>Contact 3</NavDropdown.Item>
                       </div>
                     </NavDropdown>
                   </NavDropdown>
                 </Nav>
-
-                <Form className="d-flex">
-                  <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
-                  <Button variant="outline-success">Search</Button>
-                </Form>
               </Offcanvas.Body>
             </Navbar.Offcanvas>
+            <div className={cx('header-action')}>
+              <div className={cx('account')}>
+                <i className={cx('bi bi-person', 'ha-item')}></i>
+              </div>
+              <div className={cx('search')} onClick={() => setIsSearchOpen((prev) => !prev)}>
+                <i className={cx('bi bi-search', 'ha-item')}></i>
+              </div>
+              {isSearchOpen ? (
+                <Form className={cx('d-flex', 'search-form')}>
+                  <Form.Control
+                    type="search"
+                    placeholder="Search..."
+                    className={cx('me-2', 'search-input')}
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+              ) : null}
+
+              <div className={cx('cart')} onClick={() => router.push('/cart')}>
+                <i className={cx('bi bi-cart', 'ha-item')}></i>
+                <span className={cx('cart-number')}>5</span>
+              </div>
+            </div>
           </Navbar>
         </Container>
       </header>
